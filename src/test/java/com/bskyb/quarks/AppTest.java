cc3bd1306c962ab9cd7cda4919bd047e019c89ff
Pacareu, Marc <marc.pacareu@sky.uk>
2024-05-07 13:37:35 +0100

    First commit

---
+++ src/test/java/com/bskyb/quarks/AppTest.java
@@ -0,0 +1,15 @@
+package com.bskyb.quarks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class AppTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
+
